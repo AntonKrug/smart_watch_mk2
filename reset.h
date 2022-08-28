5dcf09a382837cd93611624a0e1b794bc9845b9a
Anton Krug <anton.krug@gmail.com>
2022-08-28 13:13:28 +0100

    Moved the reset logic to a own file

---
+++ reset.h
@@ -0,0 +1,7 @@
+#ifndef SMARTWATCH_RESET_H
+#define SMARTWATCH_RESET_H
+
+// Set all the internal peripherals of the ATmega88PA with 8MHz clock into a good known state
+extern void systemPeripheralsSetup(void);
+
+#endif
