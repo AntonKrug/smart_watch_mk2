f115682f917844d2caac0602662d957f62623d08
Anton Krug <anton.krug@gmail.com>
2022-08-28 01:19:47 +0100

    Disabling Timer1 as I'm not going to try to do neopixel protocol with it

--- main.c
+++ main.c
@@ -113,27 +113,27 @@ void systemPeripheralsSetup() {
 
   // Timer/Counter 1 initialization
   // Clock source: System Clock
-  // Clock value: 125.000 kHz
-  // IRQ Timer Period: 0.5s  (2 Hz)
+  // Clock value: Timer1 Stopped
   // Mode: Normal top=0xFFFF
   // OC1A output: Disconnected
   // OC1B output: Disconnected
   // Noise Canceler: Off
   // Input Capture on Falling Edge
-  // Timer1 Overflow Interrupt: On
+  // Timer1 Overflow Interrupt: Off
   // Input Capture Interrupt: Off
   // Compare A Match Interrupt: Off
   // Compare B Match Interrupt: Off
   TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
-  TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (1<<CS11) | (1<<CS10);
-  TCNT1H=0x0B;
-  TCNT1L=0xDC;
+  TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
+  TCNT1H=0x00;
+  TCNT1L=0x00;
   ICR1H=0x00;
   ICR1L=0x00;
   OCR1AH=0x00;
   OCR1AL=0x00;
   OCR1BH=0x00;
   OCR1BL=0x00;
+  
 
   // Timer/Counter 2 initialization
   // Clock source: System Clock
@@ -152,7 +152,7 @@ void systemPeripheralsSetup() {
   TIMSK0=(0<<OCIE0B) | (0<<OCIE0A) | (1<<TOIE0);
 
   // Timer/Counter 1 Interrupt(s) initialization
-  TIMSK1=(0<<ICIE1) | (0<<OCIE1B) | (0<<OCIE1A) | (1<<TOIE1);
+  TIMSK1=(0<<ICIE1) | (0<<OCIE1B) | (0<<OCIE1A) | (0<<TOIE1);
 
   // Timer/Counter 2 Interrupt(s) initialization
   TIMSK2=(0<<OCIE2B) | (0<<OCIE2A) | (0<<TOIE2);
